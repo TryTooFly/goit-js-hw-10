8256c85ffa32c658658cb4e10976764ed4265704
Serhiy Shvayka <sergiyshvayka@gmail.com>
2022-08-14 17:51:42 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,11 @@
 export function fetchCountries(searchTerm) {
-  return fetch(`https://restcountries.com/v2/name/${searchTerm}`)
-    .then(response => {
+  return fetch(`https://restcountries.com/v2/name/${searchTerm}`).then(
+    response => {
       if (!response.ok) {
         throw new Error(response.status);
       }
       return response.json();
-    })
-    .catch(error => console.log(`${error.name}: ${error.message}`));
+    }
+  );
+  // .catch(error => console.log(`${error.name}: ${error.message}`));
 }
